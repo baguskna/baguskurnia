e6c33a93c5bcebe0a5daf50664293e2da289b089
Bagus Kurnia <bagus.kurnia@sirclo.com>
2024-08-25 17:33:04 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplate() *Templates {
+	// get current directory
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &Templates{
+		templates: template.Must(template.ParseGlob(dir + "/views/*.html")),
+	}
+}
+
+func main() {
+	// get current directory
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(dir, "dir")
+	e := echo.New()
+	e.Use(middleware.Logger())
+
+	e.Static(dir+"/images", "images")
+	e.Static("/css", "css")
+
+	e.Renderer = newTemplate()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", "")
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
